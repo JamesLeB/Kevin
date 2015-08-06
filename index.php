032735bca22fe061afc2f75a0c15531efb26e107
JamesLeB <james@jamesleblanc.org>
2015-08-06 15:59:28 -0400

    Add index file

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Kevin</title>
+		<script src='jquery-1.11.1.js'></script>
+		<script src='jquery-ui.js'></script>
+		<script src='script.js'></script>
+		<link  href='jquery-ui.css' rel='stylesheet'/>
+		<link  href='style.css' rel='stylesheet'/>
+	</head>
+	<body>
+		<div id='wrapper'>
+			<div id='header'></div>
+			<div id='main'>
+				<div id='tabs'>
+					<ul>
+						<li><a href='#tab1'>Bello</a></li>
+					</ul>
+					<div id='tab1'>?</div>
+				</div>
+			</div>
+			<div id='footer'></div>
+		</div>
+	</body>
+</html>
