156d7f58edc387b914d87161d834f59b50a4abd4
james <james@jamesleblanc.org>
2015-09-04 16:58:34 -0400

    store count and sort messages

--- kevin.php
+++ kevin.php
@@ -8,11 +8,10 @@
 	{
 		case 'start':
 			$_SESSION['tic'] = 0;
-			file_put_contents('files/test',"Test data here\n*********************");
+			$_SESSION['messages'] = array();
 			break;
 		case 'run':
 			$tic = ++$_SESSION['tic'];
-			$rtn['tic'] = $tic;
 
 			$payload = $_POST['payload'];
 			if($payload)
@@ -20,13 +19,45 @@
 				foreach($payload as $p)
 				{
 					$o = json_decode($p,true);
-					$t = $o['type'];
 					$s = $o['sequence'];
-					file_put_contents('files/test',"\n$p\n********",FILE_APPEND);
+					$_SESSION['messages'][$s] = $p;
 				}
 			}
 
-			$rtn['book'] = 'Book';
+			$cReceived = 0;
+			$cOpen = 0;
+			$cDone = 0;
+			$cMatch = 0;
+			$cError = 0;
+			file_put_contents('files/test',"Log File\n*****************");
+			$keys = array_keys($_SESSION['messages']);
+			sort($keys);
+			$cSeq = $keys[0];
+			foreach($keys as $key)
+			{
+				$m = $_SESSION['messages'][$key];
+				$o = json_decode($m,true);
+				switch($o['type'])
+				{
+					case 'received': ++$cReceived; break;
+					case 'open':     ++$cOpen;     break;
+					case 'done':     ++$cDone;     break;
+					case 'match':    ++$cMatch;    break;
+					default:         ++$cError;
+						file_put_contents('files/test',"\n$key : $m",FILE_APPEND);
+				}
+			}
+
+			$rtn['tic'] = $tic.
+				' Msgs: '     .sizeof($_SESSION['messages']).
+				' Received: ' .$cReceived.
+				' Open: '     .$cOpen.
+				' Done: '     .$cDone.
+				' Match: '    .$cMatch.
+				' Error: '    .$cError.
+				' Seq: '      .$cSeq;
+
+			$rtn['book'] = 'Where is the book?';
 
 			break;
 	}
